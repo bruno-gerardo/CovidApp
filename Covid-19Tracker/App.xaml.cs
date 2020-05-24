13a3eedf8804cc3da48444426aa806b1b8832cfb
Bruno Gerardo <gerardo.brunex@gmail.com>
2020-05-24 02:18:26 +0100

    First commit

---
+++ Covid-19Tracker/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+//using Covid_19Tracker.Services;
+//using Covid_19Tracker.Views;
+
+namespace Covid_19Tracker
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Covid-19Tracker/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Covid19Tracker.ViewModels
+{
+    public class HomePageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool isRefreshing;
+
+
+        public ICommand RefreshCommand => new Command(async () => await RefreshItemsAsync());
+
+        public bool IsRefreshing
+        {
+            get => isRefreshing;
+            set
+            {
+                isRefreshing = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("IsRefreshing"));
+            }
+        }
+
+
+        public HomePageViewModel()
+        {
+
+        }
+
+        async Task RefreshItemsAsync()
+        {
+            IsRefreshing = true;
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            
+            IsRefreshing = false;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
