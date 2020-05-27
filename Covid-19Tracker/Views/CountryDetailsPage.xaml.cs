b73e08529ce8b42fb01df9beee8c059b4587e2b3
Bruno Gerardo <gerardo.brunex@gmail.com>
2020-05-27 06:24:08 +0100

    Added CountryDetailsPage and SearchPage

---
+++ Covid-19Tracker/Views/CountryDetailsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Covid19Tracker.Views
+{
+    public partial class CountryDetailsPage : ContentPage
+    {
+        public CountryDetailsPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+        }
+
+        public ICommand BackCommand => new Command( async () => await Shell.Current.Navigation.PopAsync());
+    }
+}
---
+++ Covid-19Tracker/Views/SearchPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Covid19Tracker.Views
+{
+    public partial class SearchPage : ContentPage
+    {
+        public SearchPage()
+        {
+            InitializeComponent();
+
+            var tap = new TapGestureRecognizer();
+
+            tap.Tapped += Tap_Tapped;
+
+            cenas.GestureRecognizers.Add(tap);
+        }
+
+
+
+        private async void Tap_Tapped(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("countryDetailsPage");
+        }
+    }
+}
