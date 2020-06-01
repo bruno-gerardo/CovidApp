6371eb8fd5daf8046a1d6d77348c10596402624b
Bruno Gerardo <gerardo.brunex@gmail.com>
2020-06-01 23:04:43 +0100

    Added TimeSeries class

---
+++ Covid-19Tracker/Model/TimeSeries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Covid19Tracker.Model
+{
+    public class TimeSeriesData
+    {
+
+        [JsonProperty("confirmed")]
+        public int confirmed { get; set; }
+
+        [JsonProperty("date")]
+        public string date { get; set; }
+
+        [JsonProperty("deaths")]
+        public int deaths { get; set; }
+
+        [JsonProperty("recovered")]
+        public int recovered { get; set; }
+
+        public DateTime? DateInfo { get => DateTime.Parse(date); }
+    }
+
+    public class TimeSeries
+    {
+        [JsonProperty("count")]
+        public int count { get; set; }
+
+        [JsonProperty("result")]
+        public List<TimeSeriesData> result { get; set; }
+    }
+}
