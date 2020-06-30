f0c89faea0281ed9c61d6971cd9f8efb8543f8b5
Bruno Gerardo <gerardo.brunex@gmail.com>
2020-06-30 02:11:36 +0100

    Added shp and sbf map files; Added bookmarked gif for saved news popup; Added SavedToReadLaterPopupPage;

---
+++ Covid-19Tracker/ViewModels/Popups/SavedToReadPopupPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+
+namespace Covid19Tracker.ViewModels.Popups
+{
+    public class SavedToReadPopupPageViewModel : BaseViewModel
+    {
+        public SavedToReadPopupPageViewModel()
+        {
+            ExecuteClosePopUpCommand();
+        }
+
+        public Command ClosePopUpCommand { get; }
+
+        private async void ExecuteClosePopUpCommand()
+        {
+            await Task.Delay(1350);
+            if(PopupNavigation.Instance.PopupStack.Count != 0)
+                await PopupNavigation.Instance.PopAsync(true);
+        }
+    }
+}
---
+++ Covid-19Tracker/Views/Popups/SavedToReadPopupPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Covid19Tracker.ViewModels.Popups;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace Covid19Tracker.Views.Popups
+{
+    public partial class SavedToReadPopupPage : PopupPage
+    {
+        SavedToReadPopupPageViewModel vm;
+        public SavedToReadPopupPage()
+        {
+            InitializeComponent();
+            vm = new SavedToReadPopupPageViewModel();
+            BindingContext = vm;
+        }
+    }
+}
