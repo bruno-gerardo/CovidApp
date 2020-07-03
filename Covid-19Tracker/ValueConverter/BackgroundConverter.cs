2818a871ea80e0431ed5179fea7d64c4352de68a
Bruno Gerardo <gerardo.brunex@gmail.com>
2020-07-03 16:06:00 +0100

    Added Converter Background Color Button Chart; Added icons and flag;

---
+++ Covid-19Tracker/ValueConverter/BackgroundConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Covid19Tracker.ValueConverter
+{
+    public class BackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Color.FromHex("#e3e5ee");
+            else
+                return Color.Transparent;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
